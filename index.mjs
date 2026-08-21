e888e8c3058948250709e6b571fec992e205377b
Rafael Severgnini <romasrafael83@gmail.com>
2026-08-21 11:05:03 +0100

    add a faq page

--- index.mjs
+++ index.mjs
@@ -12,6 +12,10 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/faq", (req, res) => {
+  res.render("faq");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
